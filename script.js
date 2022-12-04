e599384fae89b4642bb7b5c24346d1d58fa54bf4
Sai Rishith <rishi.chintala19@gmail.com>
2022-12-04 17:18:03 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.getElementById('output').style.visibility='hidden';
-document.getElementById('lbsInput').addEventListener('input',function(e){
+document.getElementById('kgInput').addEventListener('input',function(e){
 document.getElementById('output').style.visibility='visible';
-let lbs=e.target.value;
-document.getElementById('gramsOutput').innerHTML=lbs/0.0022046;
-document.getElementById('kgOutput').innerHTML=lbs/2.2046;
-document.getElementById('ozOutput').innerHTML=lbs*16;
-
-
+let kgs=e.target.value;
+document.getElementById('gramsOutput').innerHTML=kgs*1000;
+document.getElementById('lbsOutput').innerHTML=kgs*2.2046226218;
+document.getElementById('ozOutput').innerHTML=kgs*35.27396195;
 });
